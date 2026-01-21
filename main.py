fbde527d881945f8a5683746982c8c8e394e137c
Athanase Deschasse <Athanase_deschasse@proton.me>
2026-01-21 13:50:12 +0000

    fix: final adjustments for GTK loop

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
 from src.engine.generator import StrikeGenerator
 from src.models.strike import LightningStrike
+from src.ui.window import StormWindow
+import sys
+import gi
+gi.require_version("Gtk", "4.0")
+from gi.repository import Gtk
+from gi.repository import GLib
 
+
+class StormApp(Gtk.Application):
+    def __init__(self):
+        super().__init__(application_id='org.storm.monitor',flags=0)
+        self.engine = StrikeGenerator()
+        self.stream = self.engine.stream()
+
+    def do_activate(self):
+        win = StormWindow(self)
+        win.present()
+        GLib.timeout_add(500, self.on_tick)
+
+
+    def on_tick(self):
+        strike = next(self.stream)
+        print(strike)
+        return True
+    
 if __name__ == "__main__":
-    print("Le projet Storm démarre")
-    system = StrikeGenerator()
-    for i in system.stream():
-        if i.is_valid():
-            print(i)
+    app = StormApp()
+    app.exit_status = app.run(sys.argv)
