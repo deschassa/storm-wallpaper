cd5bb3cc2ca9269f3d51278f3d1fbc1a0ad2fa16
Athanase Deschasse <Athanase_deschasse@proton.me>
2026-01-21 14:09:07 +0000

    feat: add drawing area

--- src/ui/window.py
+++ src/ui/window.py
@@ -6,4 +6,10 @@ class StormWindow(Gtk.ApplicationWindow):
     def __init__(self, app):
         super().__init__(application=app, title="Storm Wallpaper")
         self.set_default_size(1000, 800)
-
+        self.area = Gtk.DrawingArea()
+        self.area.set_draw_func(self.on_draw)
+        self.set_child(self.area)
+    def on_draw(self, area, context, width, height):
+        context.set_source_rgb(0, 0, 0)
+        context.paint()
+    
